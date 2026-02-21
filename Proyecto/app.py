da8f34ca5bcc81f53bf023f355cf65177f2dfd5e
Usuario <wiogualinga96@gmail.com>
2026-02-21 09:47:03 -0500

    añsdiendo estilos

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -4,17 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Página principal del portal de reservas
     return render_template('index.html')
 
 @app.route('/reservar')
 def reservar():
-    # Aquí irá el formulario para la base de datos después
     return render_template('reservar.html')
 
 @app.route('/confirmacion/<cliente>')
 def confirmacion(cliente):
-    # Ruta dinámica que recibe el nombre del cliente
+    # Esta ruta servirá para mostrar el éxito de la inserción después
     return render_template('confirmacion.html', nombre=cliente)
 
 if __name__ == '__main__':
